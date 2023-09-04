29bc822045006434c5ce0c4f01f3c71128ccfd12
kbhandpro <123443486+kbhandpro@users.noreply.github.com>
2023-09-04 17:29:36 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Tuslamj Privacy Policy</h1>
+
+      <h2>1. Introduction</h2>
+      <h4>
+        Welcome to Tuslamj ("the App"), developed and operated by Tuslamj ("we," "us," or "our"). Your privacy is important to us, and we are
+        committed to protecting your personal information. This Privacy Policy explains how we collect, use, disclose, and safeguard your personal
+        data when you use our App. By using the App, you consent to the practices described in this Privacy Policy.
+      </h4>
+      <h2>2. Information We Collect</h2>
+
+      <h4>
+        2.1. Information You Provide: When you use the App, we may collect the following information:
+        <br />
+        Personal information: such as your name, phone number, and email address when you create an account or use certain features of the App.
+        Location data: to provide emergency services, we may collect your device's location information. User-generated content: information you
+        provide when using the App, including emergency contacts and medical information you choose to share.
+        <br />
+        2.2. Automatically Collected Information: We may collect certain information automatically, including:
+        <br />
+        Device information: such as device type, operating system, and unique device identifiers. Usage data: information about how you use the App,
+        such as the features you interact with and the frequency of use. Log data: server logs and error logs that may collect data about your
+        interactions with the App.
+      </h4>
+      <h2>3. How We Use Your Information</h2>
+      <h4>
+        We use the information we collect for the following purposes:
+        <br />
+        To provide and improve the App's functionality and user experience.
+        <br />
+        To contact you for updates, support, and service-related notifications.
+        <br />
+        To provide emergency services based on your location and information you've provided.
+        <br />
+        To comply with legal obligations.
+      </h4>
+      <h2>4. Data Sharing and Disclosure</h2>
+      <h4>
+        We may share your information with the following parties:
+        <br />
+        Emergency services providers: To provide you with emergency assistance, we may share your location and information with authorized emergency
+        service providers. Service providers: We may use third-party service providers to assist with App operations, data storage, and analytics.
+        Legal requirements: We may disclose your information when required by law, court order, or government authorities.
+      </h4>
+      <h2>5. Security</h2>
+      <h4>
+        We take data security seriously and implement reasonable measures to protect your information. However, please be aware that no data
+        transmission over the internet or storage system can be guaranteed to be 100% secure.
+      </h4>
+      <h2>6. Your Choices</h2>
+      <h4>You can manage your information and preferences within the App, including updating your personal information and privacy settings.</h4>
+      <h2>7. Changes to this Privacy Policy</h2>
+      <h4>
+        We may update this Privacy Policy from time to time. When we make changes, we will update the "Last Updated" date at the beginning of this
+        policy.
+      </h4>
+      <h2>8. Contact Us</h2>
+      <h4>
+        If you have any questions or concerns about this Privacy Policy or our data practices, please contact us at contact@handpro.mn.
+        <br />
+        By using the App, you agree to this Privacy Policy and our Terms of Service.
+      </h4>
     </div>
   );
 }
